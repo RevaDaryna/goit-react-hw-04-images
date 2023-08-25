e80f1cf71644940e9a67dd6ac1e4f01c8454ed22
RevaDaryna <revadara32@gmail.com>
2023-08-26 00:23:21 +0300

    aaaaaa

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,7 +37,7 @@ export default function App(){
           catch (error) {console.log(error)} finally {setIsLoading(false)}
       }
       }
-    getImages()}, [query, page, imagesList])
+    getImages()}, [query, page])
 
   
 
